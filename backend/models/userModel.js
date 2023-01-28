c09446eacd039f03092e00988c356d2e5d45e934
albert <codewithalbert@gmail.com>
2023-01-28 11:21:25 +0200

    added signup and login functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -17,4 +19,55 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("User", userSchema)
+// static signup method
+userSchema.statics.signup = async function (username, email, password) {
+  if (!email || !password) {
+    throw new Error("All fields must be filed");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  // has the password to prvent password matching
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // create a mongoDB document for the user
+  const user = await this.create({ username, email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filed");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error(`No user found with this email ${email}`);
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error(`Passwords do not match`);
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
